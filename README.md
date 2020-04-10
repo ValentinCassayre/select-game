5aff0e6fe1ca58ff76dae235ca8e1f5ed411240d
Valentin Cassayre <valentin.cassare@gmail.com>
2020-04-10 18:10:44 +0200

    Created a windows

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # select-game
-Select is a game in python in which you need to block the opponent ant.
+Select is a game in python in which you need to block the opponent ant. 
--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 """
 Prototype game by Valentin Cassayre
 """
+
+import pygame
+pygame.init()
+
+# open a window
+pygame.display.set_caption("Select!")
+screen = pygame.display.set_mode((1080, 720))
+
+# background
+background = pygame.image.load('assets/Board.jpg')
+
+
+running = True
+
+# loop while game is open
+while running:
+    # put the background
+    screen.blit(background, (0, 0))
+
+    # update screen for background
+    pygame.display.flip()
+
+
+
+    # player close windows
+    for event in pygame.event.get():
+        # event closing
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Closing select!")
